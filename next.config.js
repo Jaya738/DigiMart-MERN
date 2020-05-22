f692f72dc56f7f0263a34335a79aa0c0702ab653
Jaya Simha Reddy Nandyala <jayasimhareddy738@gmail.com>
2020-05-22 12:47:51 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 // must restart server whenever you make changes in next.config
 module.exports = {
   env: {
-    MONGO_SRV:
-      "mongodb+srv://myApps:Jaya7382@digimart-lkadf.mongodb.net/test?retryWrites=true&w=majority",
+    MONGO_SRV:"",
     JWT_SECRET: "myToken",
-    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/digimart/image/upload",
-    STRIPE_SECRET_KEY: "sk_test_9ocoHX7fM7PewO7C6wq8TDZ900iyQq2pmW",
+    CLOUDINARY_URL: "",
+    STRIPE_SECRET_KEY: "",
   },
 };
